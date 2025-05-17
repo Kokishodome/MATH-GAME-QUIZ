cbee52565f5202445f749ff277282a89d5bb2784
slay <159949398+Kokishodome@users.noreply.github.com>
2025-05-17 10:52:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let scoreTitle=document.getElementById('scoreTitle')
 let addition=document.getElementById('addition')
 let excersize=document.getElementById('excersize')
 let answer
+let seconds=60
+let time=document.getElementById('time')
 
 
 submit.onclick=function(event){
@@ -16,12 +18,23 @@ submit.onclick=function(event){
     }
 }
 
+
+
 addition.onclick=function(){
 let firstNumber=Math.floor(Math.random()*1000)
 let secondNumber=Math.floor(Math.random()*1000)
 answer=firstNumber+secondNumber
 excersize.innerHTML=firstNumber+'+'+secondNumber
+let timeInterval=setInterval(() => {
+    seconds=seconds-1
+    if(seconds==0){
+       clearInterval(timeInterval)
+    }
+    time.innerHTML=('Time: ')+seconds
+    console.log(seconds);
+}, 1000);
 }
+
 
 subtraction.onclick=function(){
 let firstNumber=Math.floor(Math.random()*1000)
