01faa75ac9441ad5f50b75ecdf82e9ff31f1a741
slay <159949398+Kokishodome@users.noreply.github.com>
2025-05-20 19:55:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,65 @@
 console.log(11);
-let submit=document.getElementById('submit')
-let input=document.getElementById('input')
-let score=0
-let scoreTitle=document.getElementById('scoreTitle')
-let addition=document.getElementById('addition')
-let excersize=document.getElementById('excersize')
+let timeInterval
+let submit = document.getElementById('submit')
+let input = document.getElementById('input')
+let score = 0
+let scoreTitle = document.getElementById('scoreTitle')
+let addition = document.getElementById('addition')
+let excersize = document.getElementById('excersize')
 let answer
-let seconds=60
-let time=document.getElementById('time')
+let seconds = 60
+let time = document.getElementById('time')
 
 
-submit.onclick=function(event){
+submit.onclick = function (event) {
     event.preventDefault()
-    if(input.value==answer){
-
-        scoreTitle.innerHTML=('1')
+    if (input.value == answer) {
+        getExcersize()
+        score = score + 1
+        scoreTitle.innerHTML = ('Score: ') + score
     }
 }
 
-
-
-addition.onclick=function(){
-let firstNumber=Math.floor(Math.random()*1000)
-let secondNumber=Math.floor(Math.random()*1000)
-answer=firstNumber+secondNumber
-excersize.innerHTML=firstNumber+'+'+secondNumber
-let timeInterval=setInterval(() => {
-    seconds=seconds-1
-    if(seconds==0){
-       clearInterval(timeInterval)
-    }
-    time.innerHTML=('Time: ')+seconds
-    console.log(seconds);
-}, 1000);
+function getExcersize() {
+    let firstNumber = Math.floor(Math.random() * 1000)
+    let secondNumber = Math.floor(Math.random() * 1000)
+    answer = firstNumber + secondNumber
+    excersize.innerHTML = ('Excersize: ') + firstNumber + '+' + secondNumber
 }
 
 
-subtraction.onclick=function(){
-let firstNumber=Math.floor(Math.random()*1000)
-let secondNumber=Math.floor(Math.random()*1000)
-answer=firstNumber-secondNumber
-excersize.innerHTML=firstNumber+'-'+secondNumber
+addition.onclick = function () {
+    clearInterval(timeInterval)
+    getExcersize()
+    seconds = 60
+    timeInterval = setInterval(() => {
+        seconds = seconds - 1
+        if (seconds == 0) {
+            clearInterval(timeInterval)
+        }
+        time.innerHTML = ('Time: ') + seconds
+        console.log(seconds);
+    }, 1000);
 }
 
-multiplication.onclick=function(){
-let firstNumber=Math.floor(Math.random()*12)
-let secondNumber=Math.floor(Math.random()*12)
-answer=firstNumber*secondNumber
-excersize.innerHTML=firstNumber+' x '+secondNumber
+
+subtraction.onclick = function () {
+    let firstNumber = Math.floor(Math.random() * 1000)
+    let secondNumber = Math.floor(Math.random() * 1000)
+    answer = firstNumber - secondNumber
+    excersize.innerHTML = firstNumber + '-' + secondNumber
 }
 
-division.onclick=function(){
-let firstNumber=Math.floor(Math.random()*12)
-let secondNumber=Math.floor(Math.random()*12)
-answer=firstNumber-secondNumber
-excersize.innerHTML=firstNumber+' / '+secondNumber
+multiplication.onclick = function () {
+    let firstNumber = Math.floor(Math.random() * 12)
+    let secondNumber = Math.floor(Math.random() * 12)
+    answer = firstNumber * secondNumber
+    excersize.innerHTML = firstNumber + ' x ' + secondNumber
+}
+
+division.onclick = function () {
+    let firstNumber = Math.floor(Math.random() * 12)
+    let secondNumber = Math.floor(Math.random() * 12)
+    answer = firstNumber - secondNumber
+    excersize.innerHTML = firstNumber + ' / ' + secondNumber
 }
